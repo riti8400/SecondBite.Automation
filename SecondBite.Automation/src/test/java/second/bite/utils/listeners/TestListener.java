ebab4afbc66dab393d6cd468b0fa61d7cd66eee6
fadeeeed <rupeshsengar123@gmail.com>
2024-01-01 13:14:20 +0530

    Added Extent Report

---
+++ SecondBite.Automation/src/test/java/second/bite/utils/listeners/TestListener.java
@@ -0,0 +1,43 @@
+package second.bite.utils.listeners;
+
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+
+import second.bite.utils.managers.ExtentManager;
+
+public class TestListener implements ITestListener {
+    private ExtentReports extent = ExtentManager.getInstance();
+    private ThreadLocal<ExtentTest> test = new ThreadLocal<>();
+
+    @Override
+    public void onTestStart(ITestResult result) {
+        ExtentTest extentTest = extent.createTest(result.getMethod().getMethodName(),
+                result.getMethod().getDescription());
+        test.set(extentTest);
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        test.get().log(Status.PASS, "Test Passed");
+    }
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        test.get().log(Status.FAIL, "Test Failed");
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult result) {
+        test.get().log(Status.SKIP, "Test Skipped");
+    }
+
+    @Override
+    public void onFinish(ITestContext context) {
+        extent.flush();
+    }
+}
